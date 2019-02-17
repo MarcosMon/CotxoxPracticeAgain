090d2ce99b21dd743d6b3b3aad2f8b47a0383861
marc monjon <sysmarc13@hotmail.com>
2019-02-17 22:15:01 +0100

    Segundo caso test Tarifa

--- Cotxox/src/test/java/org/lasencinas/cotxox/test/TarifaTest.java
+++ Cotxox/src/test/java/org/lasencinas/cotxox/test/TarifaTest.java
@@ -14,6 +14,12 @@ public class TarifaTest {
 		assertEquals(10.4625, Tarifa.getCosteMilla(distancia), 0);
 		
 	}
+	@Test
+	public void comprobarCosteTiempo() {
+		double tiempoEsperadoMinutos = 10;
+		double total = 3.5;
+		assertEquals(total, Tarifa.getCosteTiempo(tiempoEsperadoMinutos),0);
+	}
 
 	
 
